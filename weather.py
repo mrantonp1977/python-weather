c06be64f1c364d6c3750026062d407a27197fa6e
Antonis Papaioannou <mrantonp@gmail.com>
2023-12-13 23:34:23 +0200

    athens

--- weather.py
+++ weather.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 
-def get_current_weather(city="Larissa"):
+def get_current_weather(city="Athens"):
 
     request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=metric'
 
